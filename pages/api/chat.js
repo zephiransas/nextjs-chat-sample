c51667d4434c9383582c4a79861003d7ca701002
Takafumi Yoshida <zephiransas@gmail.com>
2023-11-14 11:27:25 +0900

    @azure/openaiを使ってチャットできるようにする

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,3 +1,24 @@
-export default function handler(req, res) {
-  res.status(200).json({ "response" : `message=${req.body.message}` })
+import { OpenAIClient, AzureKeyCredential } from "@azure/openai"
+
+export default async function handler(req, res) {
+
+  const client = new OpenAIClient(
+    process.env.AZURE_OPENAI_URI,
+    new AzureKeyCredential(process.env.AZURE_OPENAI_API_KEY),
+    {
+      apiVersion: '2023-07-01-preview'
+    }
+  );
+
+  const resp = await client.getChatCompletions(
+    "yoshida-takafumi-gpt35t",
+    [
+      {
+        role: 'user',
+        content: req.body.message,
+      }
+    ]
+  )
+
+  res.status(200).json({ "response" : resp.choices[0].message.content })
 }
